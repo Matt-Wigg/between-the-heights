731004b581a7891113af66bacae60042b9510d3c
Matt-Wigg <m.p.wigglesworth@gmail.com>
2022-10-31 17:52:19 -0700

    carousel framework

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import styles from './page.module.css';
-
+import ImageCarousel from '../components/carousel';
 export default function Home() {
   return (
     <div className={styles.container}>
-      <main className={styles.main}>Coming Soon...</main>
+      <main className={styles.main}>
+        <ImageCarousel />
+      </main>
     </div>
   );
 }
